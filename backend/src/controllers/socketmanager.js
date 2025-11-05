51d0af56a322a93b6d57a8700c665493ffac09b7
khushi-7488 <khushiitiwari88@gmail.com>
2025-11-05 23:50:56 +0530

    socket

--- backend/src/controllers/socketmanager.js
+++ backend/src/controllers/socketmanager.js
@@ -9,16 +9,54 @@ const connectToSocket = (server) => {
     const io = new Server(server);
     io.on("connection", (socket) => {
         socket.on("join-call", (path) => {
-if(connections[path]===undefined){
-connections[path].push(socket.id)
-}
-connections[path].push(socket.id)
-timeOnline[socket.id] = new Date();
+            if (connections[path] === undefined) {
+                connections[path].push(socket.id)
+            }
+            connections[path].push(socket.id)
+            timeOnline[socket.id] = new Date();
+            // connections[path].forEach((elem, index)=>{
+
+            // })
+
+            for (let a = 0; a < connections[path].legth; i++) {
+                io.to(connections[path][a]).emit("user-joined", socket.id, connections[path])
+            }
+            if (messages[path] !== undefined) {
+                for (let a = 0; a < messages[path].legth; i++) {
+                    io.to(socket.id).emit("chat-message", messages[path][a]["data"],
+                        messages[path][a]["sender"], messages[path][a]["socket-id-sender"]
+                    )
+                }
+            }
         })
         socket.on("signal", (toId, message) => {
             io.to(toId).emit("signal", socket.id, message);
         })
+        socket.on("chat-message", (data, sender) => {
+            const [matchingRoom, found] = Object.entries(connections)
+                .reduce(([room, isFound], [roomKey, roomValue]) => {
+                    if (!isFound && roomValue.includes(socket.id)) {
+                        return [roomKey, true]
+                    }
+                    return [room, isFound];
+                }, ['', false]);
+            if (found === true) {
+                if (messages[matchingRoom] === undefined) {
+                    messages[matchingRoom] = []
+                }
+                messages[matchingRoom].push({ 'sender': sender, "data": data, "socket-id-sender": socket.id })
+                console.log("message", key, ":", sender, data)
+
+                connections[matchingRoom].forEach((elem) => {
+                    io.to(elem).emit("chat-message", data, sender, socket.id)
+                })
+            }
+        })
+
+
     })
 }
 
 export default connectToSocket;
+
+
